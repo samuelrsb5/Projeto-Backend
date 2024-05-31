86771dcc6f7f3d5f2537d20aa337e3043b89ef5b
samuelrsb5 <samuelcomgeorge@gmail.com>
2024-05-31 01:09:43 -0300

    Envio projeto backend ao GitHub

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CadastrarComponent } from './pages/cadastrar/cadastrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'home/:category', component: HomeComponent
+  },
+  {
+    path: 'cadastrar', component: CadastrarComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import { HomeComponent } from './pages/home/home.component';
+// import { CadastrarComponent } from './pages/cadastrar/cadastrar.component';
+
+// const routes: Routes = [
+//   {
+//     path: 'home', component: HomeComponent
+//   },
+//   {
+//     path: 'cadastrar', component: CadastrarComponent
+//   }
+  
+// ];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,67 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CadastrarComponent } from './pages/cadastrar/cadastrar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProdutosComponent } from './pages/produtos/produtos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CadastrarComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProdutosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+// import { NgModule } from '@angular/core';
+// import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { HomeComponent } from './pages/home/home.component';
+// import { HeaderComponent } from './components/header/header.component';
+// import { FooterComponent } from './components/footer/footer.component';
+// import { Routes } from '@angular/router';
+// import { CadastrarComponent } from './pages/cadastrar/cadastrar.component';
+
+
+// const routes: Routes = [
+//   {
+    
+//   }
+// ]
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     HomeComponent,
+//     HeaderComponent,
+//     FooterComponent,
+//     CadastrarComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule
+//   ],
+//   providers: [
+//     provideClientHydration()
+//   ],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
